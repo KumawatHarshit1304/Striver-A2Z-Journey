fbd3162610c93f39fe9871a07ded8482e3a10395
KumawatHarshit1304 <kumawatharshit130804@gmail.com>
2025-12-15 15:26:42 +0530

    Create LeftRotateArrayByDPlaces.java

---
+++ Arrays/LeftRotateArrayByDPlaces.java
@@ -0,0 +1,21 @@
+//https://takeuforward.org/plus/dsa/problems/left-rotate-array
+
+class LeftRotateArrayByDPlaces{
+    static void rotateArr(int[] arr, int k){
+        k = k%arr.length;
+        reverse(arr, 0, k-1);
+        reverse(arr, k, arr.length-1);
+        reverse(arr, 0, arr.length-1);
+    }
+    
+    static void reverse(int[] arr, int start, int end){
+        while(start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+
+        }
+    }
+}
